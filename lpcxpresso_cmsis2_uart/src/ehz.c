3f29ea888b413e687e56d4c532b0b186cf8300af
huy <huy@DOTHINKPAD>
2012-08-02 12:46:04 +0200

    refactored ehz parser into own files

---
+++ lpcxpresso_cmsis2_uart/src/ehz.c
@@ -0,0 +1,63 @@
+/*
+ * ehz.c
+ *
+ *  Created on: Aug 2, 2012
+ *      Author: huy
+ */
+
+#include "LPC17xx.h"
+#include "type.h"
+#include "uart.h"
+#include "ehz.h"
+
+/* we're looking for pattern  "1*255(" */
+const uint8_t search_pattern[SEARCH_PATTERN_LENGTH] = {0x31,0x2A,0x32,0x35,0x35,0x28};
+uint8_t search_match = 0;
+uint8_t serialbuffer[SERIAL_BUFFER_SIZE];
+uint8_t serialbuffer_index = 0;
+
+volatile uint8_t ehz_value_parsed = 0;
+uint32_t ehz_value = 0;
+
+void ehz_process_serial_data(uint8_t data) {
+	if (search_match >= SEARCH_PATTERN_LENGTH) {
+
+		// here comes the data
+		if (serialbuffer_index >= SERIAL_BUFFER_SIZE) {
+			// error (should not happen)
+			serialbuffer_index = 0;
+			search_match = 0;
+		}
+		serialbuffer[serialbuffer_index++] = data;
+		if (serialbuffer_index >= EHZ_VALUE_LENGTH || data == ')') {
+
+			// we're expecting 11 bytes of data
+			// * parse data here *
+			uint8_t i = 0;
+			uint8_t d;
+			// atoi conversion, ignoring non-digits
+			ehz_value = 0;
+			for (;i<serialbuffer_index;i++) {
+				d = serialbuffer[i];
+				if (d >= '0' && d <= '9') {
+					d -= '0';
+					ehz_value *= 10;
+					ehz_value += d;
+				}
+			}
+
+			// reset buffer
+			serialbuffer_index = 0;
+			search_match = 0;
+			ehz_value_parsed = 1;
+		}
+	}
+	else {
+		if (data == search_pattern[search_match]) {
+			search_match++;
+		}
+		else {
+			search_match = 0;
+		}
+	}
+}
--- lpcxpresso_cmsis2_uart/src/main.c
+++ lpcxpresso_cmsis2_uart/src/main.c
@@ -30,72 +30,21 @@
 #include "LPC17xx.h"
 #include "type.h"
 #include "uart.h"
+#include "ehz.h"
 #include "leds.h"
 #include <stdio.h>
 #include <string.h>
 
-#define SERIAL_BUFFER_SIZE 12
-#define SEARCH_PATTERN_LENGTH 6
-#define EHZ_VALUE_LENGTH 11
 
 extern volatile uint32_t UART2Count;
 extern volatile uint8_t UART2Buffer[BUFSIZE];
+extern volatile uint8_t ehz_value_parsed;
+extern uint32_t ehz_value;
 volatile uint32_t msTicks; // counter for 1ms SysTicks
-
-/* we're looking for pattern  "1*255(" */
-const uint8_t search_pattern[SEARCH_PATTERN_LENGTH] = {0x31,0x2A,0x32,0x35,0x35,0x28};
-uint8_t search_match = 0;
-uint8_t serialbuffer[SERIAL_BUFFER_SIZE];
-uint8_t serialbuffer_index = 0;
-uint32_t ehz_value = 0;
-uint8_t ehz_value_parsed = 0;
 
 //  SysTick_Handler - just increment SysTick counter
 void SysTick_Handler(void) {
   msTicks++;
-}
-
-void process_serial_data(uint8_t data) {
-	if (search_match >= SEARCH_PATTERN_LENGTH) {
-
-		// here comes the data
-		if (serialbuffer_index >= SERIAL_BUFFER_SIZE) {
-			// error (should not happen)
-			serialbuffer_index = 0;
-			search_match = 0;
-		}
-		serialbuffer[serialbuffer_index++] = data;
-		if (serialbuffer_index >= EHZ_VALUE_LENGTH || data == ')') {
-
-			// we're expecting 11 bytes of data
-			// * parse data here *
-			uint8_t i = 0;
-			uint8_t d;
-			// atoi conversion, ignoring non-digits
-			ehz_value = 0;
-			for (;i<serialbuffer_index;i++) {
-				d = serialbuffer[i];
-				if (d >= '0' && d <= '9') {
-					d -= '0';
-					ehz_value *= 10;
-					ehz_value += d;
-				}
-			}
-
-			// reset buffer
-			serialbuffer_index = 0;
-			search_match = 0;
-			ehz_value_parsed = 1;
-		}
-	}
-	else {
-		if (data == search_pattern[search_match]) {
-			search_match++;
-		}
-		else {
-			search_match = 0;
-		}
-	}
 }
 
 /*****************************************************************************
@@ -133,8 +82,9 @@ int main (void)
 
 			int i = 0;
 			for(; i < UART2Count; i++) {
-				process_serial_data(UART2Buffer[i]);
+				ehz_process_serial_data(UART2Buffer[i]);
 			}
+			UART2Count = 0;
 
 
 			if (ehz_value_parsed > 0) {
@@ -145,7 +95,6 @@ int main (void)
 			}
 
 
-			UART2Count = 0;
 			LPC_UART2->IER = IER_THRE | IER_RLS | IER_RBR;		/* Re-enable RBR */
 		}
 		else {
