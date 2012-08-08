280556003c8fb2c47be74484a08e99279b8d2ece
huy <huy@DOTHINKPAD>
2012-08-08 12:46:25 +0200

    added logs fixed estimate calculation

--- lpcxpresso_cmsis2_uart/src/main.c
+++ lpcxpresso_cmsis2_uart/src/main.c
@@ -42,10 +42,19 @@ extern volatile uint8_t UART2Buffer[BUFSIZE];
 extern volatile uint8_t ehz_value_parsed;
 extern uint32_t ehz_value;
 volatile uint32_t msTicks; // counter for 1ms SysTicks
+uint32_t old_ehz_value = 0;
+uint32_t last_ehz_value = 0;
+uint32_t last_ehz_msTicks = 0;
+
 
 //  SysTick_Handler - just increment SysTick counter
 void SysTick_Handler(void) {
   msTicks++;
+}
+
+
+uint32_t clock_time() {
+	return msTicks;
 }
 
 /*****************************************************************************
@@ -56,8 +65,8 @@ int main (void)
 {
 
 	// INIT SYSTICK
-	// Setup SysTick Timer to interrupt at 1 msec intervals
-	if (SysTick_Config(SystemCoreClock / 1000)) {
+	// Setup SysTick Timer to interrupt at 10 msec intervals
+	if (SysTick_Config(SystemCoreClock / 100)) {
 	    while (1);  // Capture error
 	}
 
@@ -77,6 +86,9 @@ int main (void)
 	logger_logStringln("logger online ...");
 	led2_off();
 	volatile int currentms = msTicks ;
+	UARTSendStringln(2, "wait 3s ...");
+	while(msTicks - currentms < 300);
+	UARTSendStringln(2, "done ...");
 
 	/* Loop forever */
 	while (1)
@@ -100,21 +112,73 @@ int main (void)
 			UART2Count = 0;
 
 			if (ehz_value_parsed > 0) {
-				SENSOR_DATA* sd = get_sensor(SENSOR_TYPE_EHZ, 0);
-				if (sd) {
-					sd->value = ehz_value;
+				if (old_ehz_value == 0) {
+					old_ehz_value = ehz_value;
 				}
+				if (ehz_value >= old_ehz_value) {
+					old_ehz_value = ehz_value;
+					uint32_t current_msTicks = clock_time(); // one tick equals 10ms see lpc17xx_systick.h
+					SENSOR_DATA* sd = get_sensor(SENSOR_TYPE_EHZ, 0);
+					if (sd) {
+						sd->value = ehz_value;
+						logger_logString("main: ehz value: ");
+						logger_logNumberln(ehz_value);
 
-				logger_logString("\n\rZaehlerstand:");
-				logger_logNumber(ehz_value);
-				logger_logCRLF();
+						if (last_ehz_value == 0) {
+							last_ehz_value = ehz_value;
+							last_ehz_msTicks = current_msTicks;
+						}
+						else {
+							uint32_t diff = 0;
+							if (current_msTicks > last_ehz_msTicks) {
+								diff = current_msTicks - last_ehz_msTicks;
+							}
+							else {
+								// check for timer overflow
+								diff = UINT32_MAX - last_ehz_msTicks + current_msTicks;
+							}
+							logger_logString("diff ticks: ");
+							logger_logNumberln(diff);
+							uint32_t diffv = ehz_value - last_ehz_value;
+							logger_logString("diff value: ");
+							logger_logNumberln(diffv);
+							// 10 seconds
+							if (diff > 1000) {
+								uint32_t estimated = diffv * 360000 / diff;
+
+								/* update value */
+								sd->value2 = estimated;
+
+								logger_logString("main: estimated ehz value: ");
+								logger_logNumberln(estimated);
+
+								last_ehz_msTicks = current_msTicks;
+								last_ehz_value = ehz_value;
+							}
+						}
+					}
+				}
+				else {
+					// log error
+					// value is expected to be greater than previous value
+					SENSOR_DATA* sd = get_sensor(SENSOR_TYPE_EHZ, 0);
+					if (sd) {
+						sd->errors++;
+						logger_logString("main: error count: ");
+						logger_logNumberln(sd->errors);
+					}
+					logger_logString("main: unexpected ehz value: ");
+					logger_logNumber(ehz_value);
+					logger_logString(" old value: ");
+					logger_logNumberln(old_ehz_value);
+				}
 				ehz_value_parsed = 0;
 			}
 
 			LPC_UART2->IER = IER_THRE | IER_RLS | IER_RBR;		/* Re-enable RBR */
 		}
 		else {
-			if (msTicks - currentms > 50) {
+			if (msTicks - currentms > 5) {
 				led2_off();
 			}
 		}
