eb94ab5c9a141e24515ee85c17cf5c8b7244074f
huy <huy@DOTHINKPAD.(none)>
2012-08-09 10:45:13 +0200

    renamed folder

--- lpcxpresso_cmsis2_uIP/main/version.h
+++
@@ -1,9 +0,0 @@
-#ifndef __VERSION_H
-#define __VERSION_H
-
-#define VERSION_MAJOR 1
-#define VERSION_MINOR 1
-#define VERSION_BUILD_ID "20120809-103852"
-#define PRODUCT_NAME "EHZ, SO, MODBUS COUNTER"
-
-#endif /* end __VERSION_H */
