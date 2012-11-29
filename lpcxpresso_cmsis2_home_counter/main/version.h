4ead80a012578056c57fcd1ac179ee689a98e0ea
Huy Do <huy.do@gmx.de>
2012-11-29 09:44:21 +0100

    new version 1.3

--- lpcxpresso_cmsis2_home_counter/main/version.h
+++ lpcxpresso_cmsis2_home_counter/main/version.h
@@ -2,10 +2,9 @@
 #define __VERSION_H
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 1
-#define VERSION_BUILD_ID "20121124-214006"
+#define VERSION_MINOR 3
+#define VERSION_BUILD_ID "20121129-094404"
 #define PRODUCT_NAME "EHZ, SO, MODBUS COUNTER"
 
-//uint32_t boot_up_counter = 0;
 
 #endif /* end __VERSION_H */
